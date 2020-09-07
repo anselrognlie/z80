ffa8a2dfdb0eec696beeb17c67e89da6a16df70e
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-09-06 19:28:29 -0700

    Generates bit opcodes

---
+++ src/ext/gen-bit-ops.js
@@ -0,0 +1,46 @@
+const regs = [
+  'b',
+  'c',
+  'd',
+  'e',
+  'h',
+  'l',
+  'ptr_hl',
+  'a',
+];
+
+const ops = [
+  'rlc',
+  'rrc',
+  'rl',
+  'rr',
+  'sla',
+  'sra',
+  'sll',
+  'srl',
+]
+
+const outGen = (op) => {
+  console.log(`gen.generate('${op}');`);
+};
+
+const genBitOp = (op) => {
+  for (let bit of [0, 1, 2, 3, 4, 5, 6, 7]) {
+    for (let reg of regs) {
+      outGen(`${op}_${bit}_${reg}`);
+    }
+    console.log();
+  }
+};
+
+for (let op of ops) {
+  for (let reg of regs) {
+    outGen(`${op}_${reg}`);
+  }
+
+  console.log();
+}
+
+genBitOp('bit');
+genBitOp('res');
+genBitOp('set');
